c9b63058436aa92fa04c3b6bfa58f54d5ad30428
Vitaliy <theyamshikov@yandex.ru>
2021-10-03 15:05:50 +0600

    init

--- deploy.php
+++ deploy.php
@@ -2,8 +2,8 @@
 
 namespace Deployer;
 
-require_once __DIR__ . '/src/Deployer/bootstrap.php';
-require_once __DIR__ . '/src/Deployer/config.php';
+require_once __DIR__ . '/src/Deployer/config/bootstrap.php';
+require_once __DIR__ . '/src/Deployer/config/config.php';
 require_once __DIR__ . '/src/Deployer/Tasks/develop.php';
 //require_once __DIR__ . '/vendor/deployer/deployer/recipe/common.php';
 
