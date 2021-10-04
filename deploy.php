66f2823580de5fab10ffd85aae46a8bca9b78895
Vitaliy <theyamshikov@yandex.ru>
2021-10-04 21:34:46 +0600

    upd

--- deploy.php
+++ deploy.php
@@ -2,8 +2,11 @@
 
 namespace Deployer;
 
+use App\Deployer\Helpers\LoaderHelper;
+
 require_once __DIR__ . '/src/Deployer/config/bootstrap.php';
-require_once __DIR__ . '/src/Deployer/config/config.php';
-require_once __DIR__ . '/src/Deployer/Tasks/develop.php';
-require_once __DIR__ . '/src/Deployer/Tasks/deploy.php';
 require_once __DIR__ . '/vendor/deployer/deployer/recipe/common.php';
+require_once __DIR__ . '/src/Deployer/recipe/init_ssh.php';
+
+$taskDir = __DIR__ . '/src/Deployer/Tasks';
+LoaderHelper::loadTasks($taskDir);
