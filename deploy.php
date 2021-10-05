398d9ad0d1a56f82272aaf2bc49193c33d899948
Vitaliy <theyamshikov@yandex.ru>
2021-10-05 23:17:20 +0600

    upd

--- deploy.php
+++ deploy.php
@@ -3,8 +3,6 @@
 namespace Deployer;
 
 require_once __DIR__ . '/vendor/zntool/deployer/src/config/bootstrap.php';
-//require_once __DIR__ . '/vendor/deployer/deployer/recipe/common.php';
 
-App::loadTasks(__DIR__ . '/src/Deployer/Tasks');
-App::initVars();
-App::initSshConnect();
+App::init();
+App::loadTasks(__DIR__ . '/src/Deployer/recipe');
