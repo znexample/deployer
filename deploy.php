b2f78ef1e5d8a62ee1bd8faee6fdf08a140c7654
Vitaliy <theyamshikov@yandex.ru>
2021-10-07 15:50:16 +0600

    init

--- deploy.php
+++ deploy.php
@@ -12,3 +12,13 @@ require_once __DIR__ . '/vendor/zntool/deployer/src/recipe/tools.php';
 //requireLibs(__DIR__ . '/src/Deployer/recipe');
 
 App::init();
+App::initVarsFromArray([
+    'repository' => 'git@gitlab.com:casino-zero/tournament.git',
+    'branch' => 'clean',
+    'public_directory' => 'public_html',
+    'keep_releases' => 3,
+    'allow_anonymous_stats' => 1,
+    'git_tty' => 1,
+    'application' => 'mysite',
+    'default_stage' => 'staging',
+]);
