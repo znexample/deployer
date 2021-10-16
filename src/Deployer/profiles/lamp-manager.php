5bc57f552bef7c2bad6f14733134976e8c0441fa
Vitaliy <theyamshikov@yandex.ru>
2021-10-16 20:09:37 +0600

    init

--- src/Deployer/profiles/lamp-manager.php
+++ src/Deployer/profiles/lamp-manager.php
@@ -5,14 +5,19 @@ return [
     'branch' => 'main',
     'deploy_path' => '/var/www/znproject/lamp-manager',
     'release_path' => '/var/www/znproject/lamp-manager',
-    'release_public_path' => '{{release_path}}/public',
-    'deploy_public_path' => '{{current_path}}/public',
+    //'release_public_path' => '{{release_path}}/public',
+    //'deploy_public_path' => '{{current_path}}/public',
 
     'deploy_var_path' => '{{deploy_path}}/var',
     'release_var_path' => '{{release_path}}/var',
-    'current_path' => '{{deploy_path}}/current',
+    //'current_path' => '{{deploy_path}}/current',
 
-    'domain' => 'lamp.tool',
+    'domains' => [
+        [
+            'domain' => 'lamp.tool',
+            'directory' => '{{current_path}}/public',
+        ],
+    ],
 
     'application' => 'LAMP manager',
     'permissions' => [
--- src/Deployer/profiles/sandbox.php
+++ src/Deployer/profiles/sandbox.php
@@ -5,14 +5,19 @@ return [
     'branch' => 'develop',
     'deploy_path' => '/var/www/php-lab/sandbox',
     'release_path' => '/var/www/php-lab/sandbox',
-    'release_public_path' => '{{release_path}}/public',
-    'deploy_public_path' => '{{current_path}}/public',
+    //'release_public_path' => '{{release_path}}/public',
+    //'deploy_public_path' => '{{current_path}}/public',
 
     'deploy_var_path' => '{{deploy_path}}/var',
     'release_var_path' => '{{release_path}}/var',
-    'current_path' => '{{deploy_path}}/current',
+    //'current_path' => '{{deploy_path}}/current',
 
-    'domain' => 'sandbox.lab',
+    'domains' => [
+        [
+            'domain' => 'sandbox.lab',
+            'directory' => '{{current_path}}/public',
+        ],
+    ],
 
     'application' => 'Sandbox lab',
     'permissions' => [
